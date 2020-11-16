0bf1983d81ca64acc72b13bf99b0ed9b197f345f
ProyectoAEF <73623402+ProyectoAEF@users.noreply.github.com>
2020-11-15 21:30:13 -0500

    carrito(a medias :v)

--- app/src/main/java/com/example/drugstore/fragments/ShoppingCartFragment.kt
+++ app/src/main/java/com/example/drugstore/fragments/ShoppingCartFragment.kt
@@ -6,10 +6,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.drugstore.*
+import kotlinx.android.synthetic.main.card_layout_cart.*
 import kotlinx.android.synthetic.main.fragment_shopping_cart.*
 
 class ShoppingCartFragment : Fragment() {
@@ -17,6 +20,8 @@ class ShoppingCartFragment : Fragment() {
     private lateinit var adapter: CartAdapter
     var carritoNombre:String = ""
     var carritoPrecio:String = ""
+
+
     private val viewModel by lazy { ViewModelProviders.of(this).get(MainViewModel::class.java) }
 
     override fun onCreateView(
@@ -25,9 +30,6 @@ class ShoppingCartFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_shopping_cart, container, false)
-
-        carritoNombre = arguments?.getString("NombreProduc").toString()
-        carritoPrecio = arguments?.getString("PrecioProduc").toString()
 
         return view
     }
@@ -40,9 +42,21 @@ class ShoppingCartFragment : Fragment() {
         adapter = CartAdapter(this)
         recyclerView.adapter = adapter
 
+        carritoNombre = arguments?.getString("NombreProduc").toString()
+        carritoPrecio = arguments?.getString("PrecioProduc").toString()
+
+        var producto = Producto ("Default","Default",carritoNombre,carritoPrecio,"default",false)
+
+        val listadata = mutableListOf<Producto>()
+        listadata.add(producto)
+
+        adapter.setListData(listadata)
+        adapter.notifyDataSetChanged()
+
         buy.setOnClickListener() {
             val intent = Intent(context, PayWindow::class.java)
             startActivity(intent)
         }
+
     }
 }
